86f96e4c204dd28800f590f2b2b26dd63bec12b7
Dmitry Romanov <romanovda@gmail.com>
2017-06-21 14:35:20 -0400

    Geometry loader update

--- js/js/GluexGeometryLoader.js
+++ js/js/GluexGeometryLoader.js
@@ -7,6 +7,8 @@ THREE.GluexGeometryLoader.prototype = {
 
     constructor: THREE.GluexGeometryLoader,
 
+
+
     group: new THREE.Group(),
     defines: {},
     geometries: {},
@@ -30,6 +32,7 @@ THREE.GluexGeometryLoader.prototype = {
         var janaGeo = JSON.parse(text);
         this.importedGeometry = janaGeo;
 
+        var toRad = Math.PI/180.0;
         var scope = this;
 
         // FCAL
@@ -45,32 +48,119 @@ THREE.GluexGeometryLoader.prototype = {
 
         // FTOF
         var ftofJson = janaGeo['/SITE/HALL/FTOF'];
-        var shape = ftofJson.shape;
+        shape = ftofJson.shape;
         var ftofGeom = new THREE.BoxGeometry(shape.x, shape.y, shape.z);
         var ftofMaterial = new THREE.MeshLambertMaterial({ color: 0xa3bad2, transparent: true, opacity: 0.5, side: THREE.DoubleSide });
         var ftofMesh = new THREE.Mesh(ftofGeom, ftofMaterial);
         ftofMesh.position.set(ftofJson.position[0], ftofJson.position[1], ftofJson.position[2]);
-        //ftofMesh.rotation.set(ftofJson.rotation[0], ftofJson.rotation[1], ftofJson.rotation[2]);
+        ftofMesh.rotation.set(ftofJson.rotation[0]*toRad, ftofJson.rotation[1]*toRad, ftofJson.rotation[2]*toRad);
         ftofMesh.name = "FTOF";
         this.group.add(ftofMesh);
 
         // BCAL
+        var lassJson = janaGeo['/SITE/HALL/LASS'];
 
-        var lassJson = janaGeo['/SITE/HALL/LASS/BCAL'];
-        var lassGeom = this.tubeGeometryHelper(lassJson.shape);
-        var lassMaterial = new THREE.MeshLambertMaterial({ color: 0xa3bad2, transparent: true, opacity: 0.5, side: THREE.DoubleSide });
-        var lassMesh = new THREE.Mesh(lassGeom, lassMaterial);
-        lassMesh.position.set(lassJson.position[0], lassJson.position[1], lassJson.position[2]);
-        lassMesh.rotation.set(Math.PI/2, 0, 0);
-        lassMesh.name = "BCAL";
-        this.group.add(lassMesh);
+        var bcalJson = janaGeo['/SITE/HALL/LASS/BCAL'];
+        var bcalGeom = this.tubeGeometryHelper(bcalJson.shape);
+        var bcalMaterial = new THREE.MeshLambertMaterial({ color: 0xa3bad2, transparent: true, opacity: 0.5, side: THREE.DoubleSide });
+        var bcalMesh = new THREE.Mesh(bcalGeom, bcalMaterial);
 
+        // BCAL position as a superposition of BCAL and LASS objects
+        var pos = {
+            x: bcalJson.position[0] + lassJson.position[0],
+            y: bcalJson.position[1] + lassJson.position[1],
+            z: bcalJson.position[2] + lassJson.position[2]
+        };
 
+        // BCAL rotation as a superposition of BCAL and LASS objects
+        var rot = {
+            x: (bcalJson.rotation[0] + lassJson.rotation[0])*toRad,
+            y: (bcalJson.rotation[1] + lassJson.rotation[1])*toRad,
+            z: (bcalJson.rotation[2] + lassJson.rotation[2])*toRad
+        };
+
+        //bcalMesh.position.set(bcalJson.position[0], bcalJson.position[1], bcalJson.position[2]);
+        bcalMesh.position.set(pos.x, pos.y, pos.z);
+        bcalMesh.rotation.set(rot.x, rot.y, rot.z);
+        bcalMesh.name = "BCAL";
+        this.group.add(bcalMesh);
+
+        // CDC
+        var cdcJson = janaGeo['/SITE/HALL/LASS/CDC'];
+        shape = cdcJson.shape;
+        var cdcTubes = {
+            0: this.tubeFromPolyConeShape(shape, 0, 1),
+            1: this.tubeFromPolyConeShape(shape, 2, 3),
+            3: this.tubeFromPolyConeShape(shape, 4, 5)
+        };
+
+        for(var i=0; i<cdcTubes.length; i++) {
+            cdcTubes[i].updateMatrix();
+        }
+        var cdcGeom = new THREE.Geometry();
+        cdcGeom.matrix.setPosition()
+
+        var cdcTube2 = this.tubeFromPolyConeShape(shape, 0, 1);
+        var cdcGeom = new THREE.BoxGeometry(shape.x, shape.y, shape.z);
+        var cdcMaterial = new THREE.MeshLambertMaterial({ color: 0xa3bad2, transparent: true, opacity: 0.5, side: THREE.DoubleSide });
+        var cdcMesh = new THREE.Mesh(cdcGeom, cdcMaterial);
+        cdcMesh.position.set(cdcJson.position[0], cdcJson.position[1], cdcJson.position[2]);
+        cdcMesh.rotation.set(cdcJson.rotation[0]*toRad, cdcJson.rotation[1]*toRad, cdcJson.rotation[2]*toRad);
+        cdcMesh.name = "FTOF";
+        this.group.add(cdcMesh);
 
         // Global shift due to... global geometry shift
         this.group.position.set(-150.501, 349.986, 500);
 
         return this.group;
+    },
+
+    tubeFromPolyConeShape: function(shape, startZPlaneIndex, endZPlaneIndex)
+    {
+        // Note: need to handle units
+        var aunit = shape['aunit'];
+        var lunit = shape['lunit'];
+
+        name = shape['name'];
+        //console.log(type, name];
+
+        var rmin = shape.zplanes[startZPlaneIndex]['rmin'];
+        var rmax = shape.zplanes[startZPlaneIndex]['rmax'];
+        var z = Math.abs(shape.zplanes[endZPlaneIndex].z - shape.zplanes[startZPlaneIndex].z);
+
+        var startphi = shape['startphi'];
+        var deltaphi = shape['deltaphi'];
+
+        if ( aunit === 'deg' ) {
+            startphi *= Math.PI/180.0;
+            deltaphi *= Math.PI/180.0;
+        }
+        return this.tubeGeometry(rmin, rmax, z, startphi, deltaphi);
+
+    },
+
+    tubeGeometry: function(rmin, rmax, z, startphi, deltaphi)
+    {
+        var shape = new THREE.Shape();
+        // x,y, radius, startAngle, endAngle, clockwise, rotation
+        shape.absarc(0, 0, rmax, startphi, deltaphi, false);
+
+        if ( rmin > 0.0 ) {
+            var hole = new THREE.Path();
+            hole.absarc(0, 0, rmin, startphi, deltaphi, true);
+            shape.holes.push(hole);
+        }
+
+        var extrudeSettings = {
+            amount : z,
+            steps : 1,
+            bevelEnabled: false,
+            curveSegments: 24
+        };
+
+        var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+        geometry.center();
+        return geometry;
     },
 
     tubeGeometryHelper: function (solid)
@@ -93,30 +183,6 @@ THREE.GluexGeometryLoader.prototype = {
             startphi *= Math.PI/180.0;
             deltaphi *= Math.PI/180.0;
         }
-
-        var shape = new THREE.Shape();
-        // x,y, radius, startAngle, endAngle, clockwise, rotation
-        shape.absarc(0, 0, rmax, startphi, deltaphi, false);
-
-        if ( rmin > 0.0 ) {
-
-            var hole = new THREE.Path();
-            hole.absarc(0, 0, rmin, startphi, deltaphi, true);
-            shape.holes.push(hole);
-
-        }
-
-        var extrudeSettings = {
-            amount : z,
-            steps : 1,
-            bevelEnabled: false,
-            curveSegments: 24
-        };
-
-        var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-        geometry.center();
-        return geometry;
-
-
+        return this.tubeGeometry(rmin, rmax, z, startphi, deltaphi);
     },
 };
