d37806e121795e5a7b0bfb0658f95e5c5e4ebfbc
T-Britton <tbritton@jlab.org>
2017-07-14 10:19:17 -0400

    cleaned up tracks. Added start_time and swim points are now 4 vectors with the last point being the flight time to that point from the beginning at t=0.

--- EventReader/Tracking.h
+++ EventReader/Tracking.h
@@ -46,12 +46,15 @@ public:
             rt->Reset();
             //auto Track_ps = new TEvePointSet();
 
-            rt->SetMass(ChargedTracks[i]->Get_BestTrackingFOM()->mass());
+
+            double mass=ChargedTracks[i]->Get_BestTrackingFOM()->mass();
+            rt->SetMass(mass);
             //rt.SetMass(TrackCandidates[i]->mass());
             auto PID=ChargedTracks[i]->Get_BestTrackingFOM()->PID();
             auto position = ChargedTracks[i]->Get_BestTrackingFOM()->position();
             auto momentum = ChargedTracks[i]->Get_BestTrackingFOM()->momentum();
             auto charge = ChargedTracks[i]->Get_BestTrackingFOM()->charge();
+            auto start_time = ChargedTracks[i]->Get_BestTrackingFOM()->t0();
 
             double TrackChiSq_NDF=ChargedTracks[i]->Get_Hypothesis(PID)->dChiSq_Track/double(ChargedTracks[i]->Get_Hypothesis(PID)->dNDF_Track);
 
@@ -60,6 +63,7 @@ public:
             DReferenceTrajectory::swim_step_t* steps =rt->swim_steps;
 
             vector<DVector3> track_points;
+            vector<double> track_point_times;
 
             for(int j=0; j<rt->Nswim_steps; j++)
             {
@@ -73,25 +77,17 @@ public:
                     continue;
 
                 track_points.push_back(step_loc);
-
+                track_point_times.push_back(steps[j].t);//flight time since start
             }
 
            // event_out<<WriteTrackJSON2(name, momentum,charge,TrackChiSq_NDF, track_points);
-            jsonTracks.emplace_back(WriteTrackJSON2(name, momentum,charge,TrackChiSq_NDF, track_points));
+            jsonTracks.emplace_back(WriteTrackJSON(name, momentum,charge,TrackChiSq_NDF,start_time, track_points, track_point_times));
             track_points.clear();
+            track_point_times.clear();
             //delete track_points;
-
-           /* if(i!=ChargedTracks.size()-1)
-                event_out<<"},"<<endl;
-            else
-                event_out<<"}"<<endl;*/
-
 
         }
 
-        //event_out<<"]"<<endl;
-        //string outstr = event_out.str();
-        //return tao::json::to_string(tao::json::value( {{ "charged_tracks", jsonTracks }}), 4);
 
         return jsonTracks;
 
@@ -114,12 +110,14 @@ public:
 
             rt->SetMass(NeutralTracks[i]->Get_BestFOM()->mass());
             //rt.SetMass(TrackCandidates[i]->mass());
+            auto start_time = NeutralTracks[i]->Get_BestFOM()->t0();
             auto momentum=NeutralTracks[i]->Get_BestFOM()->momentum();
             rt->Swim(NeutralTracks[i]->Get_BestFOM()->position(), momentum, NeutralTracks[i]->Get_BestFOM()->charge());
             //rt.Swim(TrackCandidates[i]->position(), TrackCandidates[i]->momentum(), TrackCandidates[i]->charge());
             DReferenceTrajectory::swim_step_t* steps =rt->swim_steps;
 
             vector<DVector3> track_points;
+            vector<double> track_point_times;
 
             for(int j=0; j<rt->Nswim_steps; j++)
             {
@@ -132,82 +130,42 @@ public:
                     continue;
 
                 track_points.push_back(step_loc);
-
+                track_point_times.push_back(steps[j].t);
             }
             //event_out<<tao::json::to_string(WriteTrackJSON2(name,momentum, 0,-1, track_points), 4);
 
-            jsonTracks.emplace_back(WriteTrackJSON2(name,momentum, 0,-1, track_points));
+            jsonTracks.emplace_back(WriteTrackJSON(name,momentum, 0,-1, start_time,track_points,track_point_times));
             track_points.clear();
-
-            //if(i!=NeutralTracks.size()-1)
-             //   event_out<<"},"<<endl;
-            //else
-             //   event_out<<"}"<<endl;
-
+            track_point_times.clear();
         }
 
-        //event_out<<"]"<<endl;
-        //string outstr = event_out.str();
-        //return tao::json::to_string(tao::json::value( { {"neutral_tracks", jsonTracks} }), 4);
+
         return jsonTracks;
 
     }
-    void WriteTrackJSON(ofstream& event_out, string id, TVector3 momentum, double charge, double TrackChiSq_NDF, vector<DVector3> track_points)
-    {
-
-        if(track_points.size()!=0) {
-            event_out << "{" << endl;
-            event_out << "\"id\": " <<"\""<<id<<"\"" << "," << endl; //JSON
-            event_out << "\"charge\": " << charge << "," << endl; //JSON
-            event_out << "\"TrackChiSq_NDF\": " << TrackChiSq_NDF << "," << endl; //JSON
-            event_out << "\"momentum\": " <<momentum.Mag()<<","<<endl;//<< "[" << momentum.X() << "," << momentum.Y() << "," << momentum.Z() << "],"<<endl; //JSON
-            event_out << "\"points\": " << "[" << endl; //JSON
-
-            for (int j = 0; j<track_points.size(); j++) {
-
-                if (j != track_points.size()-1) {
-                    event_out << "[" << track_points[j].X() << "," << track_points[j].Y() << "," << track_points[j].Z()
-                              << "],"
-                              << endl; //JSON
-                } else {
-                    event_out << "[" << track_points[j].X() << "," << track_points[j].Y() << "," << track_points[j].Z()
-                              << "]" << endl; //JSON
-                }
-            }
-
-            event_out<<"]"<<endl; //JSON
-        }
-    }
-    tao::json::value WriteTrackJSON2( string id, TVector3 momentum, double charge, double TrackChiSq_NDF, vector<DVector3> track_points)
+    tao::json::value WriteTrackJSON( string id, TVector3 momentum, double charge, double TrackChiSq_NDF, double start_time, vector<DVector3> track_points, vector<double> track_point_times)
     {
         tao::json::value track(
                 {
                         {"id", id},
                         {"charge", charge},
                         {"TrackChiSq_NDF", TrackChiSq_NDF},
-                        {"momentum", momentum.Mag()}
+                        {"momentum", momentum.Mag()},
+                        {"mass", momentum.Mag()},
+                        {"start_time", start_time}
 
                 });
 
         std::ostringstream ss;
         auto jsonArray = tao::json::value::array({});
 
-
         vector<double> vals;
 
         for (int j = 0; j<track_points.size(); j++) {
             vals.push_back(track_points[j].X());
-            jsonArray.emplace_back(tao::json::value::array({track_points[j].X() , track_points[j].Y() , track_points[j].Z()}));
+            jsonArray.emplace_back(tao::json::value::array({track_points[j].X() , track_points[j].Y() , track_points[j].Z(), track_point_times[j]}));
 
-//
-//            if (j != track_points.size()-1) {
-//                ss << "[" << track_points[j].X() << "," << track_points[j].Y() << "," << track_points[j].Z() << "],";
-//            } else {
-//                ss << "[" << track_points[j].X() << "," << track_points[j].Y() << "," << track_points[j].Z() << "]"; //JSON
-//            }
         }
-
-
 
         track["points"] = jsonArray;
 
