fd0c2c9b21e12add40e289d6c8a83c3b391d3f06
Dmitry Romanov <romanovda@gmail.com>
2017-08-01 16:17:19 -0400

    Initial commit of HDvis vue

---
+++ www/src/components/ViewPort.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="ViewPort">
+
+    <div id="threeViewPort">
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {
+        hdvis: null
+      }
+    },
+    mounted: function () {
+      window.alert('ViewPort mount')
+      if (this.$hdvis) {
+        window.alert('this.$hdvis !== null && defined')
+      }
+
+      if (!this.$hdvis.isInitialized) {
+        window.alert('HDvis is not initialized')
+        // this.hdvis.speak()
+        this.$hdvis.initialize('threeViewPort')
+        this.$hdvis.animate()
+      } else {
+        window.alert('HDvis OK')
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #threeViewPort {
+    border: solid aqua;
+    width: 300px;
+    height: 300px;
+    margin: auto;
+
+  }
+</style>
