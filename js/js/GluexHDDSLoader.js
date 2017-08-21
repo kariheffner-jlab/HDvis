fdbdd0539acd6dbf9dfad9428e181e00490635c5
T-Britton <tbritton@jlab.org>
2017-08-21 09:35:31 -0400

    better display of straws.  Also it appears threejs now allows transparent things in transparent things to be seen.

--- js/js/GluexHDDSLoader.js
+++ js/js/GluexHDDSLoader.js
@@ -138,9 +138,9 @@ THREE.GluexHDDSLoader.prototype = {
         //var CDCMotherGeom = this.tubeGeometry(11,60,154.75,0,2*Math.PI);
         // var cdc = new THREE.Mesh(CDCMotherGeom, new THREE.MeshLambertMaterial({ visible:true ,color: 0x436280, transparent:true, opacity: 0.4, side: THREE.DoubleSide }));
 
-        var ShortWireGeometry =this.tubeGeometry(0,.8,154.5,0,2*Math.PI);
+        var ShortWireGeometry =this.tubeGeometry(0,.05,154.5,0,2*Math.PI);
 
-        var LongWireGeometry =this.tubeGeometry(0,.8,155.5,0,2*Math.PI);
+        var LongWireGeometry =this.tubeGeometry(0,.05,155.5,0,2*Math.PI);
 
         var CDC = new THREE.Group();
         CDC.name = "CDC";
@@ -192,7 +192,7 @@ THREE.GluexHDDSLoader.prototype = {
                 {
 
                     var material = new THREE.MeshBasicMaterial({
-                        transparent: false,
+                        transparent: true,
                         opacity: 0.5,
                         color: 0xffffff,
                         side: THREE.DoubleSide
@@ -211,7 +211,7 @@ THREE.GluexHDDSLoader.prototype = {
                 {
 
                     var materialL = new THREE.MeshBasicMaterial({
-                        transparent: false,
+                        transparent: true,
                         opacity: 0.5,
                         color: 0x00ff00,
                         side: THREE.DoubleSide
