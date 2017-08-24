a892fb19617887b726c33b2b8df6eac76e9b388d
T-Britton <tbritton@jlab.org>
2017-08-24 16:43:12 -0400

    oopse overzealous with tof

--- js/js/GluexHDDSLoader.js
+++ js/js/GluexHDDSLoader.js
@@ -618,7 +618,7 @@ THREE.GluexHDDSLoader.prototype = {
         // Go through repetitions and create modules
         for(var i=0; i< ncopy; i++){
 
-            var module = new THREE.Mesh(moduleBox, material);
+            var module = new THREE.Mesh(moduleBox, material.clone());
 
             module.name = "TOFBar_p" + planeNum + "_m" + (startIndex+i);
             if(isRight) module.name += "_r";
