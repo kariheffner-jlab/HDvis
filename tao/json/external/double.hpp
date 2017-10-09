37579dde9721ace3927d12ab6a144e80a51d5236
tbritton <tbritton@jlab.org>
2017-10-09 11:54:13 -0400

    small tweak to get it to build

--- tao/json/external/double.hpp
+++ tao/json/external/double.hpp
@@ -1113,9 +1113,9 @@ namespace json_double_conversion
          return Double(kInfinity).value();
       }
 
-      static double NaN() {
-         return (Double(kNaN).value());
-      }
+     //  static double NaN() {
+     //    return (Double(kNaN).value());
+     // }
 
    private:
       static const int kExponentBias = 0x3FF + kPhysicalSignificandSize;
