f680b67d0941923aaadf716f9bfcd14b03ce893f
HallD Operations <hdops@gluon05.jlab.org>
2018-02-13 14:37:30 -0500

    counting house

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE= -G "CodeBlocks - Unix Makefiles" ../
-cmake --build ./ --target hdvis -- -j 4
+cmake3 -DCMAKE_BUILD_TYPE= -G "CodeBlocks - Unix Makefiles" ../
+cmake3 --build ./ --target hdvis -- -j 4
 echo "HDvis built"
